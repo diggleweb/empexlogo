964849dc8fb87f6e4b04e73abb7791c2fe83423a
Andrew Forward <aforward@gmail.com>
2019-04-24 20:16:28 -0400

    Move live js into separate file

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,9 +15,4 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-
-
-// Enable LiveView client side connections
-import LiveSocket from "phoenix_live_view"
-let liveSocket = new LiveSocket("/live")
-liveSocket.connect()
+import liveSocket from "./live"
